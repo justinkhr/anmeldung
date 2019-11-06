e734e1075555a474460ca8bef697b796302fce0c
justinkhr <57436637+justinkhr@users.noreply.github.com>
2019-11-06 10:08:10 +0100

    Add files via upload

---
+++ DatabaseConnection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace Anmeldung
+{
+    class DatabaseConnection
+    {
+        private MySqlConnection _connection;
+        StringBuilder _connectionString = new StringBuilder();
+        public DatabaseConnection()
+        {
+            _connectionString.Append("SERVER=localhost;");
+            _connectionString.Append("PORT=3306;");
+            _connectionString.Append("DATABASE=anmeldung;");
+            _connectionString.Append("UID=root;");
+        }
+        public MySqlConnection Conn
+        {
+            get
+            {
+                return _connection;
+            }
+            set
+            {
+                _connection = value;
+            }
+        }
+        public void Connect()
+        {
+            _connection = new MySqlConnection(_connectionString.ToString());
+            try
+            {
+                _connection.Open();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+    }
+
+}
---
+++ Query.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Anmeldung
+{
+    class Query
+    {
+        private MySqlCommand _command;
+        public MySqlCommand Command
+        {
+            get
+            {
+                return _command;
+            }
+            set
+            {
+                _command = value;
+            }
+        }
+        public void FillList()
+        {
+            _command = new MySqlCommand("SELECT LehrerName FROM lehrer;");
+            _command.ExecuteNonQuery();
+        }
+        public int CountTeachers()
+        {
+            int counter = 0;
+            _command = new MySqlCommand("SELECT COUNT(LehrerID) FROM lehrer;");
+            counter = _command.ExecuteNonQuery();
+            return counter;
+        }
+    }
+}
