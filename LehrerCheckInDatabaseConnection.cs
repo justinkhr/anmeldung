7bb2c485ce4e2b9ab5227810b8313b6bff927dff
justinkhr <57436637+justinkhr@users.noreply.github.com>
2019-11-27 11:00:33 +0100

    Add files via upload

---
+++ LehrerCheckInDatabaseConnection.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+using System.Data;
+using System.IO;
+
+namespace Anmeldung
+{
+    class LehrerCheckInDatabaseConnection
+    {
+        private MySqlConnection _connection;
+        private MySqlCommand _command;
+        StringBuilder _connectionString = new StringBuilder();
+        public LehrerCheckInDatabaseConnection()
+        {
+            _connectionString.Append("SERVER=localhost;");
+            _connectionString.Append("PORT=3306;");
+            _connectionString.Append("DATABASE=anmeldung;");
+            _connectionString.Append("UID=root;");
+            if (!File.Exists("config.ini"))
+            {
+                File.Create("config.ini");
+                File.AppendAllText("config.ini", _connectionString.ToString());
+            }
+            else
+            {
+                File.AppendAllText("config.ini", _connectionString.ToString());
+            }
+        }
+        public MySqlConnection Conn
+        {
+            get
+            {
+                return _connection;
+            }
+            set
+            {
+                _connection = value;
+            }
+        }
+        public MySqlCommand Command
+        {
+            get
+            {
+                return _command;
+            }
+            set
+            {
+                _command = value;
+            }
+        }
+        public void Connect()
+        {
+            _connection = new MySqlConnection(_connectionString.ToString());
+            try
+            {
+                _connection.Open();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+        public void FillList(ListBox lb)
+        {
+            using (MySqlDataAdapter dataAdapter = new MySqlDataAdapter("SELECT LehrerName FROM lehrer", _connection))
+            {
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                foreach(DataRow row in dataTable.Rows)
+                {
+                    lb.Items.Add(row["LehrerName"]);
+                }
+            }
+        }
+        public void UpdateVerfügbar(string lehrerID)
+        {
+            MySqlCommand command = new MySqlCommand("UPDATE lehrer SET Verfügbar = TRUE WHERE LehrerID = '" + lehrerID.Substring(0,3) + "'", _connection);
+            command.ExecuteNonQuery();
+        }
+        public void UpdateNichtVerfügbar(string lehrerID)
+        {
+            MySqlCommand command = new MySqlCommand("UPDATE lehrer SET Verfügbar = FALSE WHERE LehrerID = '" + lehrerID.Substring(0, 3) + "'", _connection);
+            command.ExecuteNonQuery();
+        }
+    }
+}
---
+++ LehrerCheckInForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Anmeldung
+{
+    public partial class LehrerCheckInForm : Form
+    {
+        LehrerCheckInDatabaseConnection databaseConnection;
+        Form form2;
+        string listBoxValue;
+        public LehrerCheckInForm()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            databaseConnection = new LehrerCheckInDatabaseConnection();
+            databaseConnection.Connect();
+            databaseConnection.FillList(lb_lehrer);
+        }
+        public void Bu_verbinden_Click(object sender, EventArgs e)
+        {
+            listBoxValue = lb_lehrer.GetItemText(lb_lehrer.SelectedItem);
+            databaseConnection.UpdateVerfügbar(listBoxValue);
+            this.Visible = false;
+            CreateConnectedForm();
+        }
+        public void CreateConnectedForm()
+        {
+            form2 = new Form();
+            Label message = new Label();
+            Button buttonTrennen = new Button();
+
+            form2.Text = "Verbunden";
+            form2.Size = new Size(300, 150);
+
+            message.Text = "Sie sind jetzt Verbunden!";
+            message.AutoSize = true;
+            message.Location = new Point(80, 20);
+            form2.Controls.Add(message);
+
+            buttonTrennen.Text = "Trennen";
+            buttonTrennen.Location = new Point(100, 50);
+            buttonTrennen.AutoSize = true;
+            form2.Controls.Add(buttonTrennen);
+
+            buttonTrennen.Click += new System.EventHandler(ButtonTrennen_click);
+            form2.Show();
+        }
+        public void ButtonTrennen_click(object sender, EventArgs e)
+        {
+            databaseConnection.UpdateNichtVerfügbar(listBoxValue);
+            form2.Close();
+            this.Visible = true;
+        }
+        private void bu_beenden_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
